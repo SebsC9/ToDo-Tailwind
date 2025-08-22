af46298bdfb8b75a20b88b81250e26d667ad988d
Sebas C <Sebascontreras319@gmail.com>
2025-08-22 13:14:01 -0300

    ToDo Terminado
    Localstorage agregado

--- script.js
+++ script.js
@@ -9,9 +9,8 @@ form.addEventListener("submit", function(event) {
     agregarTarea();
 });
 
-function agregarTarea() {
-    const tarea = tareaInput.value.trim();
-    if (tarea !== "") {
+function agregarTarea(texto = tareaInput.value.trim(), completada = false) {
+    if (texto !== "") {
         const li = document.createElement("li");
         li.className = "flex items-center justify-between gap-2 p-2 bg-stone-700 rounded mb-2";
 
@@ -19,10 +18,11 @@ function agregarTarea() {
         leftDiv.className = "flex items-center gap-2";
 
         const span = document.createElement("span");
-        span.textContent = tarea;
+        span.textContent = texto;
 
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
+        checkbox.checked = completada;
         checkbox.className = "mr-2";
 
         checkbox.addEventListener("change", function() {
@@ -31,7 +31,12 @@ function agregarTarea() {
             } else {
                 li.classList.remove("line-through", "text-gray-800");
             }
+            guardarTareas();
         });
+
+        if (completada) {
+            li.classList.add("line-through", "text-gray-800");
+        }
 
         const eliminarBtn = document.createElement("button");
         eliminarBtn.className = "bg-red-500 text-white rounded px-2";
@@ -39,6 +44,7 @@ function agregarTarea() {
         eliminarBtn.textContent = "X";
         eliminarBtn.addEventListener("click", function() {
             listaTareas.removeChild(li);
+            guardarTareas();
         });
 
         leftDiv.appendChild(checkbox);
@@ -48,12 +54,36 @@ function agregarTarea() {
         li.appendChild(eliminarBtn);
 
         listaTareas.appendChild(li);
+
+        guardarTareas();
         tareaInput.value = "";
     } else {
         alert("Por favor, ingresa una tarea.");
     }
 }
 
+
+function guardarTareas() {
+    const tareas = [];
+    const items = listaTareas.getElementsByTagName("li");
+    for (let i = 0; i < items.length; i++) {
+        const tarea = items[i].getElementsByTagName("span")[0].textContent;
+        const completada = items[i].getElementsByTagName("input")[0].checked;
+        tareas.push({ tarea, completada });
+    }
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+function cargarTareas () {
+    const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+    tareas.forEach(t => {
+        agregarTarea(t.tarea, t.completada);
+    });
+}
+
 btnEliminarTodas.addEventListener("click", () => {
     listaTareas.innerHTML = "";
+    localStorage.removeItem("tareas");
 });
+
+cargarTareas();
